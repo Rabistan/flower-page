61597f212d844a4a151ac176f30baaab35f7677d
Rabistan <rabistan007@gmail.com>
2024-10-20 17:38:43 +0530

    Add files via upload

---
+++ css/js/main.js
@@ -0,0 +1,8 @@
+ const menu =document.querySelector ('.menu')
+ const menulist=document.querySelector('.navbar ul')
+ menu.addEventListener('click',()=>{
+  menulist.classList.toggle('showmenu')
+ });
+
+
+ 
